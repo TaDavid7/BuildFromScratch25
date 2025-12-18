34706744d87f2a816577e75f9bdb7398e5245d67
David Tanioka <dtanioka7@gmail.com>
2025-12-17 21:19:21 -0800

    query select and change filename

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #main.py
 
 from read_csv import load_csv
+from query import select, print_format
 
 def main():
     #print statements
@@ -8,6 +9,7 @@ def main():
     print("Commands:")
     print("  load <csv_path>")
     print("  rows")
+    print("  select <col1> <col2> ...")
     print("  exit")
 
     header = []
@@ -19,7 +21,7 @@ def main():
         if cmd == "exit":
             break
         
-        if cmd.startswith("load "):
+        elif cmd.startswith("load "):
             path = cmd.split(" ")[1]
             header, rows = load_csv(path)
             print("File Loaded")
@@ -27,6 +29,21 @@ def main():
 
         elif cmd == "rows":
             print("Number of rows: ", len(rows))
+
+        elif cmd.startswith("select "):
+            if not rows:
+                print("No data")
+                continue
+            
+            columns = cmd.split(" ")[1:]
+            try:
+                if len(columns) == 1 and columns[0] == "*":
+                    print(header, "\n", rows)
+                else:
+                    result = select(path, columns)
+                    print(print_format(result)) 
+            except ValueError as e:
+                print(e)
         
 if __name__ == "__main__":
     main()
--- query.py
+++ query.py
@@ -1,2 +1,39 @@
 #operations
 #filtering, aggregations, group by
+import csv
+
+def print_format(result):
+    if not result:
+        return ""
+    columns = list(result[0].keys())
+    lines = []
+    header = " | ".join(columns)
+    lines.append(header)
+    lines.append("-------------")
+    for row in result:
+        values = []
+        for col in columns:
+            values.append(row[col])
+        lines.append(" | ".join(values))
+    return "\n".join(lines)
+
+def select(path, columns):
+
+    #open file
+    with open(path, encoding="utf-8") as f:
+        #.read() converts f into string 
+        reader = csv.DictReader(f)
+        #change into set with title columns
+        titles = set(reader.fieldnames)
+        filter = set(columns)
+    
+        if not filter.issubset(titles):
+            raise ValueError("Invalid columns")
+
+        result = []
+        for row in reader:
+            selected = {}
+            for col in columns:
+                selected[col] = row[col]
+            result.append(selected)
+    return result
