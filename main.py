893c03eca38bbe6db4afe2b117af7c2fdaa0048f
Michael Hwang <michaelhwang06@gmail.com>
2025-12-17 20:43:46 -0800

    Day2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #main.py
 
 from read_csv import load_csv
+from query import count
+
 
 def main():
     #print statements
@@ -9,6 +11,7 @@ def main():
     print("  load <csv_path>")
     print("  rows")
     print("  exit")
+    print("  count <Header> <Value>")
 
     header = []
     rows = []
@@ -27,6 +30,15 @@ def main():
 
         elif cmd == "rows":
             print("Number of rows: ", len(rows))
+
+        elif cmd.startswith("count "):
+            column = cmd.split()[1]
+            value = cmd.split()[2]
+            print("Header: " + column + ", Value: " + value)
+            print(str(count(column, value, header, rows)) + " matches found")
         
+        
+        print("--------------------------------")
+
 if __name__ == "__main__":
     main()
--- query.py
+++ query.py
@@ -1,2 +1,22 @@
-#operations
-#filtering, aggregations, group by
+#query.py
+def count(column, data, header, rows):
+    index = -1
+    total = 0
+    #find column index
+    for i in range(len(header)):
+        if header[i] == column:
+            index = i
+            break
+    #user input error test
+    if index == -1:
+        print("Header not found")
+        return 0
+    target = data
+    #check for matches
+    for j in range(len(rows)):
+        if rows[j][index] == data:
+            total += 1
+    return total
+
+
+    
