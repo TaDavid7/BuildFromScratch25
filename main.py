f1ce4e8762f725c185033f593db381ec4d87d6b1
David Tanioka <dtanioka7@gmail.com>
2025-12-18 00:28:17 -0800

    messages and format

--- main.py
+++ main.py
@@ -3,15 +3,58 @@
 from read_csv import load_csv
 from query import select, print_format, count
 
+"""
+These are comments
+Find # of rows
+View specific cols
+View specific range of rows 
+Find max (greatest number) of specific column
+Find min, avg, mean
+Group by column (increasing order/dec)
+Where column value =, <=, >=, <, >, !=
+From which database
+
+Explain flair
+load database
+
+--viewing (which to see)
+
+--modifier what rows to see based on column value
+where col >(symbol) number/value 
+
+range idx idx
+
+--func--
+max col_name, min col_name, avg ..., mean.., rcount
+
+
+
+load database  --> ref database
+mandatory
+EXPLAIN VIEW/DELETE <col1> <col2> ..., WHERE <col1> <symbol> <number/value>, 
+        mandatory               optional                                
+
+RANGE <idx1> <idx2>, MAX <col>, MIN <col>, AVG <col>, MEAN <col>, RCOUNT,
+optional
+
+Delete
+INSERT <val> <val> <val>
+Explain
+UPDATE - do this later
+
+"""
+
+
+
 def main():
     #print statements
     print("Database")
     print("Commands:")
     print("  load <csv_path>")
     print("  rows")
+    print("  count <Header> <Value>")
     print("  select <col1> <col2> ...")
     print("  exit")
-    print("  count <Header> <Value>")
 
     header = []
     rows = []
@@ -29,16 +72,17 @@ def main():
             print("Columns: ", header)
 
         elif cmd == "rows":
+            
             print("Number of rows: ", len(rows))
 
         elif cmd.startswith("select "):
             if not rows:
                 print("No data")
                 continue
-            
             columns = cmd.split(" ")[1:]
+            
             try:
-                if len(columns) == 1 and columns[0] == "*":
+                if columns[0] == "*":
                     print(header, "\n", rows)
                 else:
                     result = select(path, columns)
@@ -47,6 +91,7 @@ def main():
                 print(e)
 
         elif cmd.startswith("count "):
+            
             column = cmd.split()[1]
             value = cmd.split()[2]
             print("Header: " + column + ", Value: " + value)
